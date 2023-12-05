686241deebb83444cebc71f1ba63af2c39d35826
wontae0924 <wontae0924@gmail.com>
2023-12-05 11:22:39 +0900

    first

---
+++ src/C00HelloWorld.java
@@ -0,0 +1,8 @@
+public class C00HelloWorld {
+    public static void main(String[] args) {
+//        문자열을 출력할때는 ""로 문자열을 감싼다
+        System.out.println("hello world");
+        int a = 20;
+        System.out.println(a);
+    }
+}
